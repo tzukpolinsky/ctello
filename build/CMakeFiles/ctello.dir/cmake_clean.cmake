b139d28f6b1fb6ffc4dec27a21b318dc3a077844
Jerry Abu Ayoub <jerryabuayob@gmail.com>
2023-07-19 19:28:52 +0300

    fixed path compilation error

---
+++ build/CMakeFiles/ctello.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ctello.dir/src/ctello.cpp.o"
+  "CMakeFiles/ctello.dir/src/ctello.cpp.o.d"
+  "libctello.a"
+  "libctello.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ctello.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
